2f936b6744a58d104e1880070fe8fa0ca1fb740b
Kanakaraju Gandreddi <Kanakaraju.Gandreddi@ril.com>
2020-06-20 17:31:55 +0530

    first commit

---
+++ DemoSDK/DemoSDK/DemoSDK.h
@@ -0,0 +1,19 @@
+//
+//  DemoSDK.h
+//  DemoSDK
+//
+//  Created by Kanakaraju Gandreddi on 20/06/20.
+//  Copyright © 2020 Kanakaraju Gandreddi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoSDK.
+FOUNDATION_EXPORT double DemoSDKVersionNumber;
+
+//! Project version string for DemoSDK.
+FOUNDATION_EXPORT const unsigned char DemoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoSDK/PublicHeader.h>
+
+
